79431bd6ddf04a7ce569e7b1eb71a1afcd72758a
Mohamed Romdhani <m.romdhani@businesstraining.be>
2021-02-25 16:53:46 +0100

    Fixing the add method

--- src/main/java/be/businesstraining/Calcul.java
+++ src/main/java/be/businesstraining/Calcul.java
@@ -3,7 +3,7 @@ package be.businesstraining;
 public class Calcul {
 
     public static int additionner(int a, int b) {
-        return a + b +1;
+        return a + b ;
     }
     public static int multiplier(int a, int b) {
         return a * b;
