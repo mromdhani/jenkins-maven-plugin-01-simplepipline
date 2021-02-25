5efd824b84dcb1fa7ce0b3f6efafd8050f4e2d19
Mohamed Romdhani <m.romdhani@businesstraining.be>
2021-02-25 15:26:20 +0100

    Commit initial ...

---
+++ src/main/java/be/businesstraining/App.java
@@ -0,0 +1,13 @@
+package be.businesstraining;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Bonjour la Dream Team DevOps, je suis l'app Java mavenisée" );
+    }
+}
